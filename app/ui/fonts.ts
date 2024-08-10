44bfff64e0f67f6cc02304f39ee222a4775b19c6
Sarun Daunghirun <sdsarun@outlook.com>
2024-08-10 20:44:10 +0700

    feat: initial project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Roboto, Inter, Fira_Code, Lusitana } from "next/font/google"
+
+export const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
+export const inter = Inter({ subsets: ["latin"] });
+export const firaCode = Fira_Code({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400", "700"] });
